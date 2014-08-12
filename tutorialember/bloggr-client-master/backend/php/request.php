9f6ddf6abfb1bfdf586fa6c66b589a5d6459e26f
Artur Tenorio <arturvt@gmail.com>
2014-08-12 10:10:23 -0300

    Melhorias no request PHP
    Signed-off-by: Artur Tenorio <arturvt@gmail.com>

--- tutorialember/bloggr-client-master/backend/php/request.php
+++ tutorialember/bloggr-client-master/backend/php/request.php
@@ -6,67 +6,101 @@
  * Time: 5:27 PM
  */
 
+class Requester {
+    var $json_data;
+    var $response = 'Not ok!';
+    var $isOK = false;
 
-// Fill up array with names
-$a[]="Anna";
-$a[]="Brittany";
-$a[]="Cinderella";
-$a[]="Diana";
-$a[]="Eva";
-$a[]="Fiona";
-$a[]="Gunda";
-$a[]="Hege";
-$a[]="Inga";
-$a[]="Johanna";
-$a[]="Kitty";
-$a[]="Linda";
-$a[]="Nina";
-$a[]="Ophelia";
-$a[]="Petunia";
-$a[]="Amanda";
-$a[]="Raquel";
-$a[]="Cindy";
-$a[]="Doris";
-$a[]="Eve";
-$a[]="Evita";
-$a[]="Sunniva";
-$a[]="Tove";
-$a[]="Unni";
-$a[]="Violet";
-$a[]="Liza";
-$a[]="Elizabeth";
-$a[]="Ellen";
-$a[]="Edith";
-$a[]="Wenche";
-$a[]="Vicky";
+    function __construct($jsonFile) {
+        if (file_exists($jsonFile)) {
+            $this->response = 'File found!';
+            $data = file_get_contents($jsonFile);
+            $this->json_data = json_decode($data, true);
+            $this->handleJSON();
+        }
+    }
+
+    public function handleJSON() {
+        foreach ($this->json_data as $key => $value) {
+            switch ($key) {
+                case 'pages':
+                    $this->json_data[$key]++;
+                    $this->isOK = true;
+                    break;
+                case 'people':
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    public function returnValue() {
+        if ($this->isOK == true) {
+            header('Content-Type: application/json');
+            echo json_encode($this->json_data);
+        } else {
+            echo $this->response;
+        }
+    }
+}
+
 
 // get the q parameter from URL
 $q=$_REQUEST["q"];
 $hint="";
 
-$data = file_get_contents('content.json');
-$json = json_decode($data, true);
+$file = "../../json/content.json";
+$response = 'File not found!';
 
-;
+$req = new Requester($file);
+
+$req->returnValue();
+
+//if (file_exists($file)) {
+//    $response = 'File found!';
+//    $data = file_get_contents($file);
+//    $json = json_decode($data, true);
+//    handleJSON($json);
+//
+//}
+//
+//function handleJSON($json) {
+//    foreach ($json as $key => $value) {
+//        switch ($key) {
+//            case 'page':
+//                $json[$key]++;
+//                break;
+//            case 'people':
+//
+//                break;
+//            default:
+//                break;
+//
+//        }
+//    }
+//}
+//
+//function handlePeople($people) {
+//    $contents = 'nothing';
+//    foreach($people as $key => $value) {
+//        $contents = $contents . $key;
+//    }
+//    return $contents;
+//}
+
 // lookup all hints from array if $q is different from ""
-if ($q !== "") {
+//if ($q !== "") {
+//
+//
+//}
 
-    $q=strtolower($q); $len=strlen($q);
-    foreach($a as $name) {
-        if (stristr($q, substr($name,0,$len))) {
-            if ($hint==="") {
-                $hint=$name;
-            } else {
-                $hint .= ", $name";
-            }
-        }
-    }
 
-}
+//header('Content-Type: application/json');
+//echo json_encode($json);
 
-//$contents = 'Size: '+len($json);
-
-$contents = "UHUU. Size:" + $json;
-// Output "no suggestion" if no hint were found
-// or output the correct values
-echo $hint==="" ? $contents : $hint;
+//sleep(5);
+//
+//header('Content-Type: application/json');
+//echo json_encode($json);
--- tutorialember/bloggr-client-master/js/handlebar_test.js
+++ tutorialember/bloggr-client-master/js/handlebar_test.js
@@ -44,36 +44,39 @@ var html = template(listContent);
 $('#list').append(html);
 
 
-console.log('Printing the JSON');
-$.getJSON('./json/content.json', function(data) {
-    var items = [];
-    $.each(data.people, function (key, val){
-        items.push("<li>"+val.firstName+' ' + val.lastName+"</li>");
-    });
-    $('#list').append("<h1>More bellow</h1>");
-
-    $( "<ul/>", {
-        "class": "my-new-list",
-        html: items.join( "" )
-    }).appendTo( "body" );$('#list').append();
-});
-
 
 $.ajax({
     type: "POST",
     url: "./backend/php/request.php?q=",
 
     beforeSend: function() {
-        console.log('before');
+       $("#footer").css("visibility", "visible");
     },
 
     success: function successExpandedPortfolio(response) {
-        console.log('Success! Response: ', response);
+        console.log('Success!');
+        console.log('Status: ', response.status);
+        console.log('Pages: ', response.pages);
+        var data = response;
+        var items = [];
+
+        console.log(response);
+
+        $.each(data.people, function (key, val){
+            items.push("<li>"+val.firstName+' ' + val.lastName+"</li>");
+        });
+        $('#list').append("<h1>More bellow</h1>");
+        $( "<ul/>", {
+            "class": "my-new-list",
+            html: items.join( "" )
+        }).appendTo( "body" );$('#list').append();
+        $("#footer").css("visibility", "hidden");
 
     },
 
     error: function errorExpandedPortfolio(response) {
         console.log('Error! Response: ', response);
+        $("#footer").css("background-color", "red").text("Error loading Json");
 
     },
 
@@ -81,16 +84,11 @@ $.ajax({
         console.log(response);
     },
 
-    complete: function () {
+    complete: function end() {
         console.log('Complete!');
     }
 });
 
-
-//console.log('Calling request');
-//$.getJSON('./backend/php/request.php?q=a', function(data) {
-//    console.log('Result of hint: ', data);
-//});
 
 
 
