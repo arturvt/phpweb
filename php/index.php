bc4f329cd5efb5b1a5ca484ea7bfd2706bdaeefa
Artur Tenorio <arturvt@gmail.com>
2014-08-28 10:19:29 -0300

    pegando do BD
    Signed-off-by: Artur Tenorio <arturvt@gmail.com>

--- php/index.php
+++ php/index.php
@@ -1,5 +1,29 @@
 <?php
 
 
-echo '<h1>Hello PHP! </h1>';
-echo '<h2>This is the first content</h2>';
+$link = mysql_connect('localhost', 'root', 'newpwd');
+mysql_select_db('blog_db', $link);
+$result = mysql_query('SELECT id, title FROM post', $link);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>List of Posts</title>
+    </head>
+    <body>
+        <h1>List of Posts</h1>
+        <ul>
+            <?php while ($row = mysql_fetch_assoc($result)): ?>
+            	<li>
+	                <a href="/show.php?id=<?php echo $row['id'] ?>">
+	                    <?php echo $row['title'] ?>
+	                </a>
+				</li>
+            <?php endwhile; ?>
+        </ul>
+    </body>
+</html>
+
+<?php
+mysql_close($link);
+?>
