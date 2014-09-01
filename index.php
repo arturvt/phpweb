332f515f89a44dd7212d3de24da5b99c9c2d3195
Artur Tenorio <arturvt@gmail.com>
2014-09-01 12:16:12 -0300

    Symfony ADD
    Signed-off-by: Artur Tenorio <arturvt@gmail.com>

--- index.php
+++ index.php
@@ -6,4 +6,20 @@
  * Time: 6:14 PM
  */
 
-include_once("main-page.html"); ?>
+// index.php
+require_once 'vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+$request = Request::createFromGlobals();
+$uri = $request->getPathInfo();
+if ('/' == $uri) {
+    $response = list_action();
+} elseif ('/show' == $uri && $request->query->has('id')) {
+	$response = show_action($request->query->get('id')); 
+} else {
+    $html = '<html><body><h1>Page Not Found. Symfony calls.</h1></body></html>';
+    $response = new Response($html, Response::HTTP_NOT_FOUND);
+}
+// echo the headers and send the response
+$response->send();
+?>
--- php/index.php
+++ php/index.php
@@ -1,29 +1,31 @@
-<?php
+<?php 
+// load and initialize any global libraries
+require_once 'model.php';
+require_once 'controllers.php';
+// route the request internally
+$uri = $_SERVER['REQUEST_URI'];
 
+	if ('/php/index.php' == $uri) {
+		list_action();
+	} elseif (strpos($uri, '/php/index.php/show?') !== false && isset($_GET['id'])) {
+		show_action($_GET['id']);
+	} else {
+		header('Status: 404 Not Found!');
+		echo '<html><body>';
+		echo '<h1>Page not found. Try other URL.</h1>';
+		echo '<h2>Requested URI='.$uri.'</h2>';
+		echo '<h3>GETID = '. isset($_GET['id']).'</h3>';
+		if (strpos($uri, '/php/index.php/show?') !== false) {
+			echo '<h3>STRPOS = something! </h3>';	
+		} else {
+			echo '<h3>STRPOS = NOTHING </h3>';
+		}
+		
+		echo '<h3>URi = '. $uri.'</h3>';
+		echo '</body></html>';
+	}
 
-$link = mysql_connect('localhost', 'root', 'newpwd');
-mysql_select_db('blog_db', $link);
-$result = mysql_query('SELECT id, title FROM post', $link);
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>List of Posts</title>
-    </head>
-    <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php while ($row = mysql_fetch_assoc($result)): ?>
-            	<li>
-	                <a href="/show.php?id=<?php echo $row['id'] ?>">
-	                    <?php echo $row['title'] ?>
-	                </a>
-				</li>
-            <?php endwhile; ?>
-        </ul>
-    </body>
-</html>
+	// $posts = get_all_posts();
 
-<?php
-mysql_close($link);
+	// require 'templates/list.php';
 ?>
